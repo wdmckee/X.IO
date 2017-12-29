a22517cc514d92d26f60582139ba63917783a011
Derek McKee <derek.mckee@slalom.com>
2017-12-29 10:23:31 -0600

    fixes to make beta ready

--- Arithmetic.IO/Evaluator.cs
+++ Arithmetic.IO/Evaluator.cs
@@ -2,26 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using X.IO.Common;
 using X.IO.Common.Stack;
 namespace X.IO.Arithmetic
 {
-    public class Arithmetic
+    public class Evaluator
     {
 
 
-        public Result Eval(string _inputexpr, Dictionary<string, dynamic> _external_data = null)
+        public Result Eval(string expr)
+        {
+           return  Eval(expr, null);
+        }
+
+
+        public Result Eval(string expr, Dictionary<string, dynamic> external_data = null)
         {
 
-
-            var tokens = Helper_ConvertStringToList(_inputexpr);
+            expr = Regex.Replace(expr, @"\s+", "");
+            var tokens = Helper_ConvertStringToList(expr);
             int _index = 0;
             ArithmeticParser p = new ArithmeticParser(tokens, ref _index);
 
+            if (p.arith_expression == null || p.arith_expression.expression != expr) { p.result.IsError = true; p.result.value = ""; return p.result; }
            
            
-            ArithmeticInterpreter i = new ArithmeticInterpreter(_external_data);
+            ArithmeticInterpreter i = new ArithmeticInterpreter(external_data);
             i.Result(p.result);
             p.result.value = i.ActionList[0].event_data;
 
